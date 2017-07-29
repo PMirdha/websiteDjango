375802b149ccb9fed7fc5879059f73b86d4c7490
PMirdha <prateek1991mirdha@gmail.com>
2017-07-29 18:56:56 +0530

    More Files added

--- music/views.py
+++ music/views.py
@@ -1,25 +1,42 @@
 from django.http import Http404
 from django.http import HttpResponse
 #from django.template import loader 
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render,get_object_or_404
+from .models import Album,Song
 
 def index(request):
 	all_albums=Album.objects.all();
 	#all_albums=Album.objects.filter(id=10);
-	#template=loader.get_template('music/index.html');
+	#template=loader.get_template('music/index.html');        #Shortcut used see return render()
+	#return HttpResponse(template.render(context,request))
 	context={'all_albums': all_albums,}
 	#http=""
 	#for album in all_albums:
 	#	url = str(album.id)+'/'
 	#	http+='<a href="'+url+'">'+album.album_title+"&nbsp;"+album.artist+'</a><br>'
-	#return HttpResponse(template.render(context,request))
 	return render(request,'music/index.html',context)
 
 def detail(request,album_id):
-	try:
-		album=Album.objects.get(pk=album_id)
-	except Album.DoesNotExist:
-		raise Http404("Album Does not exist")
+	# Shortcut is mentioned below
+	#try:
+	#	album=Album.objects.get(pk=album_id)
+		#songs=album.song_set.all();
+	#except Album.DoesNotExist:
+	#	raise Http404("Album Does not exist")
+	album=get_object_or_404(Album,pk=album_id)
 	return render(request,'music/detail.html',{'album': album})
+
+def favorite(request,album_id):
+	album=get_object_or_404(Album,pk=album_id)
+	try:
+		selected_song=album.song_set.get(pk=request.POST['song'])
+	except	(KeyError,Song.DoesNotExist):
+		return render(request,'music/detail.html',{
+			'album': album,
+			'error_message':"You did not select a valid song"
+			})
+	else:
+		selected_song.is_favorite='true'
+		selected_song.save()
+		return render(request,'music/detail.html',{'album': album})
 
